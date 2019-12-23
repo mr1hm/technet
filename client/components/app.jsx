df2718b3c6f4e59e402463c1c772d0f808580997
Kevin <kevinihm@gmail.com>
2019-12-23 15:12:55 -0800

    added setViewPass as prop on hardware

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -167,7 +167,7 @@ export default class App extends React.Component {
         <div className="container-fluid main">
           <Header currentView={this.state.view.name} text="Computer Hardware" setViewCart={this.setView} cartItemCount={this.state.cart.length} />
           <Categories setViewCategory={this.setView} cartItemCount={this.state.cart.length} />
-          <Hardware addToCart={this.addToCart} cartTotal={this.getCartTotal} cartSummary={this.state.cart} backToCatalog={this.setView} />
+          <Hardware setViewPass={this.setView} addToCart={this.addToCart} cartTotal={this.getCartTotal} cartSummary={this.state.cart} backToCatalog={this.setView} />
         </div>
       );
     } else if (this.state.view.name === 'misc') {
