dfddd48c5048efb6ee68ef5282615526d90b2cdb
Kevin <kevinihm@gmail.com>
2019-12-23 13:31:09 -0800

    added curly braces to if statement that checks if  is truthy to make sure it wasn't the cause of an error

--- server/public/api/cart_delete.php
+++ server/public/api/cart_delete.php
@@ -26,7 +26,9 @@ if (!empty($_SESSION['cartId'])) {
 $query = "DELETE FROM `cartItems` WHERE `cartItems`.`productID` = $id AND `cartItems`.`cartID` = $cartId";
 $result = mysqli_query($conn, $query);
 
-if (!$result) throw new Exception('delete query error ' . mysqli_error($conn));
+if (!$result) {
+  throw new Exception('delete query error ' . mysqli_error($conn));
+}
 
 print(json_encode($result));
 
