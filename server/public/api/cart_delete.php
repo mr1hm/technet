3adfbfa4a9a3037da1c5d75e1c6197782c37f8d6
Kevin <kevinihm@gmail.com>
2019-12-23 13:19:12 -0800

    added new cart_delete.php file to handle deleteFromCart functionality

---
+++ server/public/api/cart_delete.php
@@ -0,0 +1,33 @@
+<?php
+
+if (!defined('INTERNAL')) {
+  exit('will not allow direct access');
+}
+
+$bodyData = getBodyData();
+
+if (empty($bodyData['id'])) {
+  throw new Exception('did not receive ID from bodyData');
+}
+
+$id = intval($bodyData['id']);
+
+if ($id <= 0) {
+  throw new Exception('id is not greater 0');
+}
+
+if (!empty($_SESSION['cartId'])) {
+  $cartId = $_SESSION['cartId'];
+  print('cartID' . $cartId);
+} else {
+  $cartId = false;
+}
+
+$query = "DELETE FROM `cartItems` WHERE `cartItems`.`productID` = $id AND `cartItems`.`cartID` = $cartId";
+$result = mysqli_query($conn, $query);
+
+if (!$result) throw new Exception('delete query error ' . mysqli_error($conn));
+
+print(json_encode($result));
+
+?>
