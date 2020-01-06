ff8f80a3b672e48132150fec33a4a23deaca4921
Kevin <kevinihm@gmail.com>
2020-01-06 12:29:14 -0800

    added deleteItem state property to app.jsx

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -16,6 +16,7 @@ export default class App extends React.Component {
     this.state = {
       cart: [],
       cartTotal: null,
+      deleteItem: false,
       view: {
         name: 'catalog',
         params: {}
