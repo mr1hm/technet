2b25e73ec57f330e0bffbf025337178b56682250
Kevin <kevinihm@gmail.com>
2020-01-06 13:37:54 -0800

    added row to cartSummaryItem to line up images

--- client/components/cartSummaryItem.jsx
+++ client/components/cartSummaryItem.jsx
@@ -28,19 +28,21 @@ export default class CartSummaryItem extends React.Component {
     return (
       <>
         {this.state.deleteItem ? <ConfirmationModal deleteItem={this.state.deleteItem} product={this.props.product} handleDeleteItem={this.handleDeleteItemClick} cartProduct={this.props.product} deleteFromCart={this.props.deleteFromCart} productID={this.props.cartItemID} /> : null}
-        <div className="offset-2 col-8 mb-3">
-          <div className="card cartCard">
-            <div className="card-horizontal">
-              <div className="img-square-wrapper">
-                <img className="cartImageDiv" src={this.props.cartItemImage} alt="image"></img>
-              </div>
-              <div className="card-body">
-                <h4 className="card-title">{this.props.cartItemName}</h4>
-                {this.props.cartItemCount > 1 ? <p className="itemCount">QUANTITY: <button onClick={() => this.props.handleDecrement(this.props.cartItemID)} className="decrementBtn"><i className="fas fa-minus-circle decrementIcon"></i></button>{this.props.cartItemCount}<button onClick={() => this.props.handleIncrement(this.props.cartItemID)} name="increment" className="incrementBtn"><i className="fas fa-plus-circle incrementIcon"></i></button></p>
-                  : <p className="itemCount">QUANTITY: {this.props.cartItemCount}<button onClick={() => this.props.handleIncrement(this.props.cartItemID)} className="incrementBtn"><i className="fas fa-plus-circle incrementIcon"></i></button></p>}
-                <p className="cartItemPriceTag">{`$${productTotal}`}</p>
-                <p className="card-text">{this.props.cartItemInfo}</p>
-                <button className="btn btn-danger" onClick={this.handleDeleteItemClick}>Remove</button>
+        <div className="row justify-content-center">
+          <div className="col-8 mb-3">
+            <div className="card cartCard">
+              <div className="card-horizontal">
+                <div className="img-square-wrapper">
+                  <img className="cartImageDiv" src={this.props.cartItemImage} alt="image"></img>
+                </div>
+                <div className="card-body">
+                  <h4 className="card-title">{this.props.cartItemName}</h4>
+                  {this.props.cartItemCount > 1 ? <p className="itemCount">QUANTITY: <button onClick={() => this.props.handleDecrement(this.props.cartItemID)} className="decrementBtn"><i className="fas fa-minus-circle decrementIcon"></i></button>{this.props.cartItemCount}<button onClick={() => this.props.handleIncrement(this.props.cartItemID)} name="increment" className="incrementBtn"><i className="fas fa-plus-circle incrementIcon"></i></button></p>
+                    : <p className="itemCount">QUANTITY: {this.props.cartItemCount}<button onClick={() => this.props.handleIncrement(this.props.cartItemID)} className="incrementBtn"><i className="fas fa-plus-circle incrementIcon"></i></button></p>}
+                  <p className="cartItemPriceTag">{`$${productTotal}`}</p>
+                  <p className="card-text">{this.props.cartItemInfo}</p>
+                  <button className="btn btn-danger" onClick={this.handleDeleteItemClick}>Remove</button>
+                </div>
               </div>
             </div>
           </div>
