e02e3b9e25a9246039207851204fa2b115a8260d
Kevin <kevinihm@gmail.com>
2020-01-07 17:12:45 -0800

    added conditionals to query databse for placing orders

--- server/public/api/orders.php
+++ server/public/api/orders.php
@@ -1,9 +1,29 @@
 <?php
 
 header('Content-Type: application/json');
+require_once 'functions.php';
+set_exception_handler('error_handler');
+require_once 'db_connection.php';
+
+$bodyData = getBodyData();
 
 $method = $_SERVER['REQUEST_METHOD'];
 $order = file_get_contents('php://input');
+
+// if ($method === 'GET') {
+
+// }
+
+// if ($method === 'POST') {
+//   $cartID = $bodyData['cartID'];
+
+//   $query = "DELETE FROM `cartItems` WHERE `cartID` = $cartID";
+//   $result = mysqli_query($conn, $query);
+
+//   if (!$result) {
+//     throw new Exception('error deleting from cartItems ' . mysqli_error($conn));
+//   }
+// }
 
 if ($method != 'POST') {
   http_response_code(404);
