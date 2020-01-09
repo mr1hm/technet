8a0cd261cddad1963a1e698f595e1108adc8e95b
Kevin <kevinihm@gmail.com>
2020-01-08 18:57:15 -0800

    minor changes to jsx in checkoutForm

--- client/components/checkoutForm.jsx
+++ client/components/checkoutForm.jsx
@@ -98,69 +98,71 @@ export default class CheckoutForm extends React.Component {
     const { userInfo, checkout, nameValidated, cardValidated, addressValidated, cityValidated, stateValidated, zipValidated } = this.state;
     let total = this.props.cartTotal().toFixed(2);
     return (
-      <form>
+      <>
         <div className="row">
           <div className='col-12 order-total'>
             Order Total: {`$${total}`}
           </div>
         </div>
-        <br/>
-        <div className="row">
-          <div className="col">
-            <small id="emailHelp" className="checkout-font form-text text-muted">{`All fields below are required before proceeding with placing your order.`}</small>
-            <small id="emailHelp" className="checkout-font form-text text-muted">{`We'll never share your information with anyone else.`}</small>
-            <h4 className="checkout-font" style={{ marginTop: '8px', letterSpacing: '0.5px' }}>PERSONAL</h4>
-            <div className="form-group">
-              <label className="checkout-font" htmlFor="exampleInputEmail1">NAME</label>
-              <br/>
-              {nameValidated && userInfo.userName !== '' ? null : <span style={{ color: 'red', fontSize: '12px' }}>Please enter a valid name</span>}
-              <input onChange={this.handleChange} name="userName" type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Full Name"></input>
+        <br />
+        <form className="checkout-form">
+          <div className="row">
+            <div className="col payment-form">
+              <small id="emailHelp" className="checkout-font form-text text-muted">{`All fields below are required before proceeding with placing your order.`}</small>
+              <small id="emailHelp" className="checkout-font form-text text-muted">{`We'll never share your information with anyone else.`}</small>
+              <h4 className="checkout-font" style={{ marginTop: '8px', letterSpacing: '0.5px' }}>PERSONAL</h4>
+              <div className="form-group">
+                <label className="checkout-font" htmlFor="exampleInputEmail1">NAME</label>
+                <br/>
+                {nameValidated && userInfo.userName !== '' ? null : <span style={{ color: 'red', fontSize: '12px' }}>Please enter a valid name</span>}
+                <input onChange={this.handleChange} name="userName" type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Full Name"></input>
+              </div>
+              <div className="form-group">
+                <label className="checkout-font" htmlFor="exampleInputPassword1">CARD</label>
+                <br/>
+                {cardValidated && userInfo.cardNumber !== null ? null : <span style={{ color: 'red', fontSize: '12px' }}>Please enter a valid credit card number</span>}
+                <input onChange={this.handleChange} name="cardNumber" type="text" className="form-control" id="exampleInputPassword1" placeholder="Credit Card Number"></input>
+              </div>
+              <h4 className="checkout-font" style={{ letterSpacing: '0.5px' }}>SHIPPING</h4>
+              <div className="form-group">
+                <label className="checkout-font" htmlFor="userAddress">ADDRESS</label>
+                <br/>
+                {addressValidated && userInfo.userAddress !== '' ? null : <span style={{ color: 'red', fontSize: '12px' }}>Please enter a valid address (Street number and Street name)</span>}
+                <input onChange={this.handleChange} type="text" name="userAddress" className="form-control" id="userAddress" rows="3"></input>
+              </div>
+              <div className="form-group">
+                <label className="checkout-font" htmlFor="userCity">CITY</label>
+                <br />
+                {cityValidated && userInfo.userCity !== '' ? null : <span style={{ color: 'red', fontSize: '12px' }}>Please enter a valid city</span>}
+                <input onChange={this.handleChange} name="userCity" className="form-control" id="userCity" rows="3"></input>
+              </div>
+              <div className="form-group">
+                <label className="checkout-font" htmlFor="userState">STATE</label>
+                <br />
+                {stateValidated && userInfo.userState !== '' ? null : <span style={{ color: 'red', fontSize: '12px' }}>Please enter a valid state</span>}
+                <input onChange={this.handleChange} name="userState" className="form-control" id="userState" rows="3"></input>
+              </div>
+              <div className="form-group">
+                <label className="checkout-font" htmlFor="userZip">ZIP CODE</label>
+                <br />
+                {zipValidated && userInfo.userZip !== '' ? null : <span style={{ color: 'red', fontSize: '12px' }}>Please enter a valid zip code</span>}
+                <input onChange={this.handleChange} type="text" name="userZip" className="form-control" id="userZip" rows="3"></input>
+              </div>
             </div>
-            <div className="form-group">
-              <label className="checkout-font" htmlFor="exampleInputPassword1">CARD</label>
-              <br/>
-              {cardValidated && userInfo.cardNumber !== null ? null : <span style={{ color: 'red', fontSize: '12px' }}>Please enter a valid credit card number</span>}
-              <input onChange={this.handleChange} name="cardNumber" type="text" className="form-control" id="exampleInputPassword1" placeholder="Credit Card Number"></input>
-            </div>
-            <h4 className="checkout-font" style={{ letterSpacing: '0.5px' }}>SHIPPING</h4>
-            <div className="form-group">
-              <label className="checkout-font" htmlFor="userAddress">ADDRESS</label>
-              <br/>
-              {addressValidated && userInfo.userAddress !== '' ? null : <span style={{ color: 'red', fontSize: '12px' }}>Please enter a valid address (Street number and Street name)</span>}
-              <input onChange={this.handleChange} type="text" name="userAddress" className="form-control" id="userAddress" rows="3"></input>
-            </div>
-            <div className="form-group">
-              <label className="checkout-font" htmlFor="userCity">CITY</label>
-              <br />
-              {cityValidated && userInfo.userCity !== '' ? null : <span style={{ color: 'red', fontSize: '12px' }}>Please enter a valid city</span>}
-              <input onChange={this.handleChange} name="userCity" className="form-control" id="userCity" rows="3"></input>
-            </div>
-            <div className="form-group">
-              <label className="checkout-font" htmlFor="userState">STATE</label>
-              <br />
-              {stateValidated && userInfo.userState !== '' ? null : <span style={{ color: 'red', fontSize: '12px' }}>Please enter a valid state</span>}
-              <input onChange={this.handleChange} name="userState" className="form-control" id="userState" rows="3"></input>
-            </div>
-            <div className="form-group">
-              <label className="checkout-font" htmlFor="userZip">ZIP CODE</label>
-              <br />
-              {zipValidated && userInfo.userZip !== '' ? null : <span style={{ color: 'red', fontSize: '12px' }}>Please enter a valid zip code</span>}
-              <input onChange={this.handleChange} type="text" name="userZip" className="form-control" id="userZip" rows="3"></input>
+            <div className="col">
+              {checkout ? <ConfirmationModal userInfo={userInfo} handleOrder={this.handleOrder} backToCatalog={this.props.backToCatalog} placeOrder={this.props.placeOrder} checkout={checkout} /> : null}
             </div>
           </div>
-          <div className="col">
-            {checkout ? <ConfirmationModal userInfo={userInfo} handleOrder={this.handleOrder} backToCatalog={this.props.backToCatalog} placeOrder={this.props.placeOrder} checkout={checkout} /> : null}
+          <div className="row">
+            <div className="col-5 d-flex checkout-continue">
+              <button className="btn btn-warning backToListBtn mb-2" onClick={() => this.props.backToCatalog('catalog', {})}>Continue Shopping</button>
+            </div>
+            <div className="col-1 d-flex checkout-order">
+              {!nameValidated || !cardValidated || !addressValidated || !cityValidated || !stateValidated || !zipValidated ? <button onClick={e => e.preventDefault()} className="ml-auto btn btn-danger mb-2"><i className="fas fa-exclamation-circle"></i></button> : <button onClick={this.handleOrder} type="button" className="ml-auto btn btn-success mb-2">Place Order</button>}
+            </div>
           </div>
-        </div>
-        <div className="row">
-          <div className="col-5 d-flex justify-content-start">
-            <button className="btn btn-warning backToListBtn mb-2" onClick={() => this.props.backToCatalog('catalog', {})}>Continue Shopping</button>
-          </div>
-          <div className="col-1 d-flex">
-            {!nameValidated || !cardValidated || !addressValidated || !cityValidated || !stateValidated || !zipValidated ? <button onClick={e => e.preventDefault()} className="ml-auto btn btn-danger mb-2"><i className="fas fa-exclamation-circle"></i></button> : <button onClick={this.handleOrder} type="button" className="btn btn-success mb-2">Place Order</button>}
-          </div>
-        </div>
-      </form>
+        </form>
+      </>
     );
   }
 }
