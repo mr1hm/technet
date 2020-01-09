41efa704a690fb6294c308c4e816bf25b83bc3ce
Kevin <kevinihm@gmail.com>
2020-01-08 18:58:25 -0800

    added new container to create zoom-in effect for the header

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -6,24 +6,33 @@ export default class Header extends React.Component {
     this.state = {
       cartItemCount: null
     };
+    this.stopPropagation = this.stopPropagation.bind(this);
   }
+
   componentDidUpdate(prevProps) {
     const { cartItemCount } = this.props;
     if (prevProps.cartItemCount !== this.props.cartItemCount) this.setState({ cartItemCount });
   }
 
+  stopPropagation(e) {
+    e.stopPropagation();
+    this.props.setViewCart('cart', {});
+  }
+
   render() {
     const { text, setViewCart, cartItemCount } = this.props;
     return (
-      <div className="header row align-items-center">
-        <div className="col-2 headerTitle">
-          <a className="headerTitle" onClick={() => setViewCart('catalog', {})} href="#">{text}</a>
-        </div>
-        <div className="col-10 shoppingCart d-flex justify-content-end">
-          <button className="btn btn-success" onClick={() => setViewCart('cart', {})}>
-            <i className="cartIcon fas fa-shopping-cart"></i>
-            <span className="cartBadge badge badge-light">{cartItemCount}</span>
-          </button>
+      <div className="container-fluid header-container">
+        <div onClick={() => setViewCart('catalog', {})} className="header row align-items-center">
+          <div className="col-2 headerTitle">
+            {text}
+          </div>
+          <div className="col-10 shoppingCart d-flex justify-content-end">
+            <button className="btn btn-success cartBtn" onClick={e => this.stopPropagation(e)}>
+              <i className="cartIcon fas fa-shopping-cart"></i>
+              <span className="cartBadge badge badge-light">{cartItemCount}</span>
+            </button>
+          </div>
         </div>
       </div>
     );
